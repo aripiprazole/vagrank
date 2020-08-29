f4698e32c95761251d3c0add46567a24329efcc0
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-29 20:25:40 -0300

    feat: add script host

---
+++ script-host/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.lorenzoog"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(project(":script-definition"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+
+    api(kotlin("scripting-jvm-host"))
+    api(kotlin("scripting-compiler-embeddable"))
+}
---
+++ script-host/src/main/kotlin/ScriptCompiler.kt
@@ -0,0 +1,26 @@
+package com.lorenzoog.vagrank.script.host
+
+import com.lorenzoog.vagrank.script.definition.VagrankScript
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.withContext
+import java.io.File
+
+interface ScriptCompiler {
+    suspend fun compileScript(file: File): VagrankScript
+}
+
+@Suppress("FunctionName")
+fun ScriptCompiler(): ScriptCompiler = ScriptCompilerImpl()
+
+internal class ScriptCompilerImpl : ScriptCompiler {
+    private val context = SupervisorJob() + CoroutineName("vagrank-compiler")
+    private val scope = CoroutineScope(context)
+
+    override suspend fun compileScript(file: File): VagrankScript {
+        return withContext(context) {
+            TODO()
+        }
+    }
+}

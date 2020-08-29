7e87aafa824a28e93192d9d8f34be55b12854abc
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-29 20:23:40 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.0"
+    application
+}
+
+group = "com.lorenzoog"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "vagrank"
+include("tooling")
+include("script-definition")
+include("cli")
+include("script-host")
